12c6b0515a240dc226f7859549f89121b24bc593
SzpaQ <myemail@adres.com>
2018-08-28 08:06:00 +0200

    css and html Cards

---
+++ bck.php
@@ -0,0 +1,76 @@
+<?php echo do_shortcode('
+			[skillwrapper type="bar"]
+			[skill title_background="#f7a53b" bar_foreground="#f7a53b" bar_background="#eeeeee" percent="90" title="CSS"]
+			[/skillwrapper]') ?>
+			<ul class="portfolio_skills">
+				<li>Pasja Informatyki(pl) - Kurs CSS - 270 min</li>
+				<li>Derek Banas(en) - Kurs RW in One Video - 30 min</li>
+				<li>Strona w czystym html i css - 700 podstron</li>
+			</ul>
+
+			<?php echo do_shortcode('
+			[skillwrapper type="bar"]
+			[skill title_background="#3B56A5" bar_foreground="#062FAA" bar_background="#eeeeee" percent="95" title="HTML"]
+			[/skillwrapper]') ?>
+			<ul class="portfolio_skills">
+				<li>Pasja Informatyki(pl) - Kurs HTML - 370 min</li>
+				<li>Strona w czystym html i css - 700 podstron</li>
+			</ul>
+
+			<?php echo do_shortcode('
+			[skillwrapper type="bar"]
+			[skill title_background="#fa6e6e" bar_foreground="#fa6e6e" bar_background="#eeeeee" percent="75" title="PHP"]
+			[/skillwrapper]') ?>
+
+			<ul class="portfolio_skills">
+				<li>Pasja Informatyki(pl) - Kurs PHP - 420 min</li>
+				<li>LearnWebCode(en) - Kurs Wordpress - 330 min</li>
+				<li>Strona na Wordpressie - portfolio</li>
+				<li>CodinGame (SzpaQ) - Zadania Średnie - 1</li>
+			</ul>
+
+			<?php echo do_shortcode('
+			[skillwrapper type="bar"]
+			[skill title_background="#fa6e6e" bar_foreground="#fa6e6e" bar_background="#eeeeee" percent="75" title="JavaScript"]
+			[/skillwrapper]') ?>
+
+			<ul class="portfolio_skills">
+				<li>Pasja Informatyki(pl) - Kurs JS - 270 min</li>
+			</ul>
+
+			<?php echo do_shortcode('
+			[skillwrapper type="bar"]
+			[skill title_background="#f7a53b" bar_foreground="#f7a53b" bar_background="#eeeeee" percent="90" title="MySQL"]
+			[/skillwrapper]') ?>
+			<ul class="portfolio_skills">
+				<li>Pasja Informatyki(pl) - Kurs MySQL - 180 min</li>
+			</ul>
+
+			<?php echo do_shortcode('
+			[skillwrapper type="bar"]
+			[skill title_background="#336699" bar_foreground="#336699" bar_background="#eeeeee" percent="40" title="C++"]
+			[/skillwrapper]') ?>
+
+			<ul class="portfolio_skills">
+				<li>Pasja Informatyki(pl) - Kurs C++ - 480 min</li>
+				<li>Pasja Informatyki(pl) - Kurs Podejcia Obiektowego (C++) - 380 min</li>
+				<li>Polski Spoj (Szpakusik) - Zadania Łatwe - 15</li>
+				<li>CodinGame (SzpaQ) - Zadania Średnie - 1</li>
+			</ul>
+
+
+
+			<?php echo do_shortcode('
+			[skillwrapper type="bar"]
+			[skill title_background="#336699" bar_foreground="#336699" bar_background="#eeeeee" percent="40" title="Java"]
+			[/skillwrapper]') ?>
+			<ul class="portfolio_skills">
+				<li>Derek Banas(en) - Kurs Java in One Video - 30 min</li>
+				<li>Polski Spoj (Szpakusik) - Zadania Łatwe - 1</li>
+				<li>CodinGame (SzpaQ) - Zadania Łatwe - 5</li>
+				<li>CodinGame (SzpaQ) - Zadania Średnie - 1</li>
+			</ul>
+
+			
+
+			
--- page-portfolio.php
+++ page-portfolio.php
@@ -26,96 +26,45 @@ get_header();
 		</div>
 			<?php if (have_posts()) : while (have_posts()) : the_post();?>
 			<div class="page-description">
-				
-			<?php echo do_shortcode('
-			[skillwrapper type="bar"]
-			[skill title_background="#f7a53b" bar_foreground="#f7a53b" bar_background="#eeeeee" percent="90" title="CSS"]
-			[/skillwrapper]') ?>
-			<ul class="portfolio_skills">
-				<li>Pasja Informatyki(pl) - Kurs CSS - 270 min</li>
-				<li>Derek Banas(en) - Kurs RW in One Video - 30 min</li>
-				<li>Strona w czystym html i css - 700 podstron</li>
-			</ul>
+				<div class="flip-container">
+					<div class="flip-card php"></div>
+					<div class="flip-card html"></div>
+					<div class="flip-card css"></div>
+					<div class="flip-card java"></div>
+					<div class="flip-card sql"></div>
+					<div class="flip-card cpp"></div>
+					<div class="flip-card js"></div>
+					<div class="flip-card bootstrap"></div>
+					<div class="flip-card json"></div>
+					<!-- <div class="flip-card"></div>-->
 
-			<?php echo do_shortcode('
-			[skillwrapper type="bar"]
-			[skill title_background="#3B56A5" bar_foreground="#062FAA" bar_background="#eeeeee" percent="95" title="HTML"]
-			[/skillwrapper]') ?>
-			<ul class="portfolio_skills">
-				<li>Pasja Informatyki(pl) - Kurs HTML - 370 min</li>
-				<li>Strona w czystym html i css - 700 podstron</li>
-			</ul>
 
-			<?php echo do_shortcode('
-			[skillwrapper type="bar"]
-			[skill title_background="#fa6e6e" bar_foreground="#fa6e6e" bar_background="#eeeeee" percent="75" title="PHP"]
-			[/skillwrapper]') ?>
+					<div class="clearfix"></div>
 
-			<ul class="portfolio_skills">
-				<li>Pasja Informatyki(pl) - Kurs PHP - 420 min</li>
-				<li>LearnWebCode(en) - Kurs Wordpress - 330 min</li>
-				<li>Strona na Wordpressie - portfolio</li>
-				<li>CodinGame (SzpaQ) - Zadania Średnie - 1</li>
-			</ul>
+				</div>
 
-			<?php echo do_shortcode('
-			[skillwrapper type="bar"]
-			[skill title_background="#336699" bar_foreground="#336699" bar_background="#eeeeee" percent="40" title="C++"]
-			[/skillwrapper]') ?>
+			
 
-			<ul class="portfolio_skills">
-				<li>Pasja Informatyki(pl) - Kurs C++ - 480 min</li>
-				<li>Pasja Informatyki(pl) - Kurs Podejcia Obiektowego (C++) - 380 min</li>
-				<li>Polski Spoj (Szpakusik) - Zadania Łatwe - 15</li>
-				<li>CodinGame (SzpaQ) - Zadania Średnie - 1</li>
-			</ul>
-
-			<?php echo do_shortcode('
-			[skillwrapper type="bar"]
-			[skill title_background="#336699" bar_foreground="#336699" bar_background="#eeeeee" percent="40" title="Java"]
-			[/skillwrapper]') ?>
-			<ul class="portfolio_skills">
-				<li>Derek Banas(en) - Kurs Java in One Video - 30 min</li>
-				<li>Polski Spoj (Szpakusik) - Zadania Łatwe - 1</li>
-				<li>CodinGame (SzpaQ) - Zadania Łatwe - 5</li>
-				<li>CodinGame (SzpaQ) - Zadania Średnie - 1</li>
-			</ul>
-
-			<?php echo do_shortcode('
-			[skillwrapper type="bar"]
-			[skill title_background="#fa6e6e" bar_foreground="#fa6e6e" bar_background="#eeeeee" percent="75" title="JavaScript"]
-			[/skillwrapper]') ?>
-
-			<ul class="portfolio_skills">
-				<li>Pasja Informatyki(pl) - Kurs JS - 270 min</li>
-			</ul>
-
-			<?php echo do_shortcode('
-			[skillwrapper type="bar"]
-			[skill title_background="#f7a53b" bar_foreground="#f7a53b" bar_background="#eeeeee" percent="90" title="MySQL"]
-			[/skillwrapper]') ?>
-			<ul class="portfolio_skills">
-				<li>Pasja Informatyki(pl) - Kurs MySQL - 180 min</li>
-			</ul>
+						<!-- END OF SKILLSET-->
 
 				<div class="breadcrumb breadcrumb-center">
 				<h1>Realizacje</h1>
-			</div>
-			<div class="team-fix">
-				<?php get_template_part( 'template/section_team');?>
-			</div>
-
-				<div class="breadcrumb breadcrumb-center">
-					<h1>Umiejętności interpersonalne</h1>
+				</div>
+				<div class="team-fix">
+					<?php get_template_part( 'template/section_team');?>
 				</div>
 
-			<?php echo do_shortcode('
-			[skillwrapper type="circle" track_color="#dddddd" chart_color="#333333" chart_size="150" align="center"]
-			[skill percent="45" title="Przemawianie"]
-			[skill percent="85" title="J. Angielski"]
-			[skill percent="70" title="Praca w Zespole"]
-			[skill percent="90" title="Nawiązywanie Kontaktów"]
-			[/skillwrapper]') ?>
+					<div class="breadcrumb breadcrumb-center">
+						<h1>Umiejętności interpersonalne</h1>
+					</div>
+
+				<?php echo do_shortcode('
+				[skillwrapper type="circle" track_color="#dddddd" chart_color="#333333" chart_size="150" align="center"]
+				[skill percent="45" title="Przemawianie"]
+				[skill percent="85" title="J. Angielski"]
+				[skill percent="70" title="Praca w Zespole"]
+				[skill percent="90" title="Nawiązywanie Kontaktów"]
+				[/skillwrapper]') ?>
 			</div>
 
 
